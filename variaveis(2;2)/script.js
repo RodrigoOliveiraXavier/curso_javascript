d363c20fbd89bc500b3af002173b660ad8489d06
Rodrigo Oliveira Xavier <rodrigo.oliveira.xavier151@gmail.com>
2021-09-04 11:44:32 -0300

    Aula 5, aprendendo a utilizar variaveis no JS (2/2)

---
+++ variaveis(2;2)/script.js
@@ -0,0 +1,12 @@
+const nome = {nome:'Rodrigo', sobrenome:'Xavier'};
+
+nome.nome = 'Pedro';
+
+console.log(nome);
+
+//Exemplo de uso do let
+// if (nome == 'Rodrigo') {
+//     let idade = 90;
+// }
+
+// console.log(idade);
