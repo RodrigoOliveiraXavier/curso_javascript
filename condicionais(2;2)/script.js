19e845c6467c4e97ff967b168fdcbdac36aa23ad
Rodrigo Oliveira Xavier <rodrigo.oliveira.xavier151@gmail.com>
2021-09-04 11:23:15 -0300

    Aula 4, condicionais no JS (2/2)

---
+++ condicionais(2;2)/script.js
@@ -0,0 +1,15 @@
+var hora = 18;
+
+//Exemplo de else if
+// if (hora < 12) {
+//     alert('Bom Dia!');
+// } else if (hora < 18) {
+//     alert('Boa Tarde!');
+// } else if (hora <= 23) {
+//     alert('Boa Noite!');
+// }
+
+//if com duas condicionais
+if (hora == 12 || hora == 18) {
+    alert('Você está na hora do rush!');
+}
